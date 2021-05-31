bcc34c9535efcc87586b2970ff6797ec8a69854e
Troykiri <V-Troitsky@yandex.ru>
2021-05-31 13:01:53 +0300

    connected to API

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,41 @@
+import { useState, useEffect } from 'react';
+
 import appStyles from './app.module.css';
 
-import data from '../../utils/data';
+import DATA_ID from '../../utils/constants';
 
 import AppHeader from '../app-header/app-header.js';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.js';
 import BurgerConstructor from '../burger-constructor/burger-constructor.js';
 
 function App() {
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    res: {}
+  })
+
+  function getData() {
+    setState({...state, isLoading:true})
+    fetch(DATA_ID)
+      .then(res => res.json())
+      .then(res => setState({...state, res, isLoading: false}))
+      .catch(e => setState({...state, isLoading: false, hasError: true}))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const data = state.res.data;
+  const { isLoading, hasError } = state;
+
   return (
     <main className={`${appStyles.page} mb-10`}>
       <AppHeader />
       <section className={appStyles.main}>
-        <BurgerIngredients data = {data} />
-        <BurgerConstructor data = {data} />
+        {!isLoading && !hasError && data && <BurgerIngredients data = {data} />}
+        {!isLoading && !hasError && data && <BurgerConstructor data = {data} />}
       </section>
     </main>
   )
